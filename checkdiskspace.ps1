c2faab357f7a0797cfef4df30c852a8abd37c9fc
peterhyvonen <peterhyvonen@users.noreply.github.com>
2019-02-19 15:28:32 -0800

    Add files via upload

---
+++ checkdiskspace.ps1
@@ -0,0 +1,23 @@
+﻿
+$freeSpaceNeeded = 5
+
+
+    #if less run disk clean up and check again
+    #if still less send mail
+
+Function CheckDiskSpace {
+    $drives = gwmi win32_volume -Filter 'drivetype = 3' | select driveletter, label, @{LABEL='GBfreespace';EXPRESSION={$_.freespace/1GB} }
+    foreach ($drive in $drives) {
+        if ($drive.driveletter -eq "C:") {
+            if ($drive.GBfreespace -gt $freeSpaceNeeded) {
+                write-host "More than $freeSpaceNeeded GB found on C"
+            }
+            else {
+                write-host "Not enough space on C, need to cleaning"
+            }
+        }
+    }
+ 
+} #end function 
+
+CheckDiskSpace #Check C: for 5GB of free space
